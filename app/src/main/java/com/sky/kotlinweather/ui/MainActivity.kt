5a1f651cb9db5030dc7e7fdaed7c6aeb5db677f4
shenbibo <402178946@qq.com>
2017-09-15 01:36:08 +0800

    主界面添加toolbar，添加动画效果，使用接口

--- app/src/main/java/com/sky/kotlinweather/ui/MainActivity.kt
+++ app/src/main/java/com/sky/kotlinweather/ui/MainActivity.kt
@@ -3,19 +3,23 @@ package com.sky.kotlinweather.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.View
 import com.sky.kotlinweather.R
+import com.sky.kotlinweather.R.id.*
 import com.sky.kotlinweather.domain.GetCityWeatherCommand
+import com.sky.kotlinweather.ui.interfaces.ToolbarManager
 import com.sky.slog.LogcatTree
 import com.sky.slog.Slog
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.city_weather_item.*
 import kotlinx.android.synthetic.main.city_weather_item.view.*
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.startActivity
-import org.jetbrains.anko.toast
-import org.jetbrains.anko.uiThread
+import org.jetbrains.anko.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ToolbarManager {
+
+    override val toolbar by lazy { find<Toolbar>(R.id.toolbar) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Slog.init(LogcatTree()).simpleMode(true)
@@ -30,6 +34,18 @@ class MainActivity : AppCompatActivity() {
         searchButton.setOnClickListener {
             requestCityWeather(searchTextView.text.toString())
         }
+
+        cityWeatherList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    searchContainer.visibility = View.VISIBLE
+                } else {
+                    searchContainer.visibility = View.GONE
+                }
+            }
+        })
+
+        attachToScroll(cityWeatherList)
     }
 
     private fun requestCityWeather(name: String) {
@@ -43,6 +59,8 @@ class MainActivity : AppCompatActivity() {
 
                 }
                 cityWeatherList.adapter.notifyDataSetChanged()
+
+                toolbarTitle = "${weatherList.cityName} (${weatherList.country})"
             }
         }
     }
---
+++ app/src/main/java/com/sky/kotlinweather/ui/interfaces/ToolbarManager.kt
@@ -0,0 +1,64 @@
+package com.sky.kotlinweather.ui.interfaces
+
+import android.appwidget.AppWidgetHost
+import android.graphics.drawable.Drawable
+import android.support.v7.graphics.drawable.DrawerArrowDrawable
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.View
+import com.sky.kotlinweather.R
+import com.sky.kotlinweather.base.App
+import com.sky.kotlinweather.extensions.ctx
+import com.sky.kotlinweather.extensions.slideEnter
+import com.sky.kotlinweather.extensions.slideExit
+import org.jetbrains.anko.toast
+
+/**
+ * 操纵toolbar的公共接口
+ * 详述类的功能。
+ * Created by sky on 2017/9/15.
+ */
+interface ToolbarManager {
+    val toolbar: Toolbar
+
+    var toolbarTitle: String
+        get() = toolbar.title.toString()
+        set(value) {
+            toolbar.title = value
+        }
+
+    fun initToolbar(){
+        toolbar.inflateMenu(R.menu.menu_main)
+        toolbar.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.actionSettings -> App.instance.toast("setting clicked")
+                else -> App.instance.toast("unknow operate")
+            }
+            true
+        }
+    }
+
+    /**
+     * 设置oolbarbar图标和点击事件
+     * */
+    fun enableHomeAsUp(up:(View) ->Unit){
+        toolbar.navigationIcon = createArrowIcon()
+        toolbar.setNavigationOnClickListener { up(it) }
+    }
+
+    private fun createArrowIcon() = with(DrawerArrowDrawable(toolbar.ctx)){
+        progress = 1f
+        this
+    }
+
+    /**
+     * 设置滑动时的动画效果
+     * */
+    fun attachToScroll(recyclerView: RecyclerView){
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                if(dy > 0) toolbar.slideExit() else toolbar.slideEnter()
+            }
+        })
+    }
+}
